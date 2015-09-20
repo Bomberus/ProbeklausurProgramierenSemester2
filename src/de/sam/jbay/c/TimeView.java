d95dc81357009c8fd9986b84bb8ae3ea99227768
Pascal Maximilian Bremer <bremer.pascal@student.dhbw-karlsruhe.de>
2015-09-20 20:12:07 +0200

    Update TimeView.java
    Make an active state to stop the thread and prevent him from running forever

--- src/de/sam/jbay/c/TimeView.java
+++ src/de/sam/jbay/c/TimeView.java
@@ -4,29 +4,38 @@ import javax.swing.*;
 import java.awt.*;
 import java.util.Calendar;
 
+private boolean active = false;
+
+public boolean isActive() {
+    return active;
+}
+
+public void setActive(boolean active) {
+    this.active = active;
+
+    if (active) {
+            new Thread(this).start();
+    }
+}
+
 /**
  * Created by samuel on 15.07.15.
  */
 public class TimeView extends JLabel implements Runnable {
     public TimeView() {
         updateText();
-        new Thread(this).start();
+        //new Thread(this).start();
     }
 
 
     @Override
     public void run() {
-        while (true) {
+        while (active) {
             try {
                 Thread.sleep(100);
             } catch (InterruptedException e) {
             }
-            SwingUtilities.invokeLater(new Runnable() {
-                @Override
-                public void run() {
-                    updateText();
-                }
-            });
+            SwingUtilities.invokeLater(this::updateText); //Power of Java 8 :D
         }
     }
 
